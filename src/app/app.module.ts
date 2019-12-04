454f34ec83dfeb88787e769ce79ec263eb571e57
kris10ansn <40371637+kris10ansn@users.noreply.github.com>
2019-12-04 15:35:33 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,18 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { HTTP } from '@ionic-native/http/ngx';
+
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    entryComponents: [],
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        HTTP,
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { DistanceService } from '../services/distance.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss']
 })
 export class HomePage {
+	public percent: number = null;
+	public dist: number = null;
 
-  constructor() {}
+	constructor(private distance: DistanceService) {
+		this.distance.get().then(dist => this.set(dist));
+	}
 
+	public refresh(event) {
+		this.distance.get().then(dist => {
+			this.set(dist);
+			event.target.complete();
+		});
+	}
+
+	public set(cm) {
+		const { min, round } = Math;
+		const dec = n => round(n * 100) / 100;
+
+		const MAX_FOOD = 30;
+		const DIST = min(cm, MAX_FOOD);
+		const PERCENT_EATEN = round((DIST / MAX_FOOD) * 100);
+
+		this.percent = 100 - PERCENT_EATEN;
+		this.dist = dec(cm);
+	}
 }
