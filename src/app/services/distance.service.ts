f692343dee89db9987d7caf9d865ed915b68af75
kris10ansn <40371637+kris10ansn@users.noreply.github.com>
2019-12-04 15:35:33 +0100

    initial commit

---
+++ src/app/services/distance.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HTTP } from '@ionic-native/http/ngx';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class DistanceService {
+	private readonly url = 'http://92.221.76.131:8080/';
+
+	constructor(private http: HTTP) {}
+
+	public async get() {
+		const response = await this.http.get(this.url, {}, {});
+		return Number(response.data);
+	}
+}
